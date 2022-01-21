156a110148066febd9bd418fac0cfed424dd3d6f
Cormac Kantargis <cormac.kantargis.pro@gmail.com>
2022-01-21 10:44:31 -0500

    feat(ranking): Update ranking api response format

--- api/controllers/rank.controller.js
+++ api/controllers/rank.controller.js
@@ -2,21 +2,23 @@ const httpStatus = require('http-status');
 const { getRanksMap } = require('../helpers/rank.helper');
 
 exports.getRanks = async (req, res, next) => {
-  try {
-    const { page: pageStr, limit: limitStr } = req.query;
-    let page = parseInt(pageStr, 10);
-    let limit = parseInt(limitStr, 10);
-    if (isNaN(page)) page = 0;
-    if (isNaN(limit)) limit = 10;
+  const { page: pageStr, limit: limitStr } = req.query;
+  let page = parseInt(pageStr, 10);
+  let limit = parseInt(limitStr, 10);
+  if (isNaN(page)) page = 0;
+  if (isNaN(limit)) limit = 10;
 
+  try {
     const ranksMap = await getRanksMap();
     const rankedPlayers = Object.values(ranksMap);
-    const ranks = rankedPlayers.sort(
+    const totalRanks = rankedPlayers.sort(
       (playerA, playerB) => playerA.rank - playerB.rank,
     );
-    res
-      .status(httpStatus.OK)
-      .json({ ranks: ranks.slice(page * limit, page * limit + limit) });
+    const total = totalRanks.length;
+    const ranks = totalRanks.slice(page * limit, page * limit + limit);
+    res.status(httpStatus.OK).json({
+      result: { total, page, limit, ranks },
+    });
   } catch (err) {
     next(err);
   }
