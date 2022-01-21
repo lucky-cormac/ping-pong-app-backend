a7bb887a4c569dd5e4ec3bc2bf7b011b08f8f9ee
Cormac Kantargis <cormac.kantargis.pro@gmail.com>
2022-01-21 03:46:29 -0500

    feat(ranking): Update ranking api handler

--- api/controllers/rank.controller.js
+++ api/controllers/rank.controller.js
@@ -3,8 +3,20 @@ const { getRanksMap } = require('../helpers/rank.helper');
 
 exports.getRanks = async (req, res, next) => {
   try {
+    const { page: pageStr, limit: limitStr } = req.query;
+    let page = parseInt(pageStr, 10);
+    let limit = parseInt(limitStr, 10);
+    if (isNaN(page)) page = 0;
+    if (isNaN(limit)) limit = 10;
+
     const ranksMap = await getRanksMap();
-    res.status(httpStatus.OK).json({ ranksMap });
+    const rankedPlayers = Object.values(ranksMap);
+    const ranks = rankedPlayers.sort(
+      (playerA, playerB) => playerA.rank - playerB.rank,
+    );
+    res
+      .status(httpStatus.OK)
+      .json({ ranks: ranks.slice(page * limit, page * limit + limit) });
   } catch (err) {
     next(err);
   }
